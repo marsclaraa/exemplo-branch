db2be55b7786d92e4cbb4308119cc4eb2a964d6a
marsclaraa <mariaclaralopesdemaria@gmail.com>
2025-03-20 09:47:44 -0300

    adicionado funcionalidades na controller

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -2,35 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tarefa;
 use Illuminate\Http\Request;
 
 class TarefaController extends Controller
 {
+
+    public function index()
+    {
+        return Tarefa::all();
+    }
+
     public function store(Request $request)
     {
-
+        $tarefa = Tarefa::create($request->all()); //request->all, pega todos os dados da model, para não perecisar do 
+        return response()->json($tarefa,201);
     }
-
-    public function index(Request $request)
-    {
-
-    }
-
 
     public function show(Request $request)
     {
-
+        return Tarefa::find($id);
     }
 
     public function update(Request $request, string $id) // o id vai ser do tipo string
     {
-
+        $tarefa = Tarefa::find($id);
+        $tarefa->update($request->all());
+        return response()->json($tarefa,200);
     }
 
     public function destroy(string $id)
     {
+        $tarefa = Tarefa::find($id)->delete();
+        return response()->json(null,204);
 
     }
-
-
 }
